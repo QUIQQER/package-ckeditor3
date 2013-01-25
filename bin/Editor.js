97eaea5ad53b97905a4d33d7212ab985faa44a47
Henning Leutz <leutz@pcsg.de>
2013-01-25 14:17:16 +0100

    Plugin CKEditor -> begin

---
+++ bin/Editor.js
@@ -0,0 +1,28 @@
+/**
+ * CKEditor3 for QUIQQER
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ *
+ * @module package/ckeditor3/bin/Editor
+ * @package package/ckeditor3/bin/Editor
+ */
+
+define('package/ckeditor3/bin/Editor', [
+
+    'classes/Editor'
+
+], function(Editor)
+{
+    console.log( 'package/ckeditor3/bin/Editor' );
+
+    return new Class({
+
+        Implements : [ Editor ],
+        Type       : 'QUI.package.CKEditor3',
+
+        options : {
+
+        }
+    });
+
+});
