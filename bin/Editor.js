97a2574ae8876f21db14e44968d925272eecd660
Henning Leutz <leutz@pcsg.de>
2013-01-25 15:03:55 +0100

    v1

--- bin/Editor.js
+++ bin/Editor.js
@@ -7,13 +7,13 @@
  * @package package/ckeditor3/bin/Editor
  */
 
-define('package/ckeditor3/bin/Editor', [
+define('package/quiqqer/ckeditor3/bin/Editor', [
 
     'classes/Editor'
 
 ], function(Editor)
 {
-    console.log( 'package/ckeditor3/bin/Editor' );
+    console.log( 'package/quiqqer/ckeditor3/bin/Editor' );
 
     return new Class({
 
